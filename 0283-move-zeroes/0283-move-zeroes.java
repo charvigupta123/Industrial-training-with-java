cf31edaded0034bd0118b84abee26e370d9ab3df
charvigupta123 <146472962+charvigupta123@users.noreply.github.com>
2026-02-13 16:01:20 +0530

    Time: 2 ms (90.02%), Space: 47.7 MB (75.02%) - LeetHub

---
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        
+        int insertPos = 0;
+        
+        // Move non-zero elements forward
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nums[insertPos] = nums[i];
+                insertPos++;
+            }
+        }
+        
+        // Fill remaining positions with 0
+        while (insertPos < nums.length) {
+            nums[insertPos] = 0;
+            insertPos++;
+        }
+    }
+}
